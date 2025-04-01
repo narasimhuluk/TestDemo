6bad5cc710d3afea9db22dae6ade4c97f360f575
narasimha <narasimha.k@intense.in>
2025-04-01 20:56:35 +0530

    second commit

---
+++ src/test/java/NKTech/TESTRahul/DemosTest.java
@@ -0,0 +1,16 @@
+package NKTech.TESTRahul;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemosTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("www.google.com");
+
+	}
+
+}
